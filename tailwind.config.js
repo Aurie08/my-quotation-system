d8b0225ffbf7a6db6314a47f7ae02a4f47dddf00
Olinda <vashiolinda@gmail.com>
2025-07-07 07:02:05 -0700

    feat: Configure Tailwind CSS with Rydberg Starck brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'rs-dark-navy': '#111C24', // From RGB 17-28-36, WEBSAFE 111C24
+        'rs-teal-green': '#126262', // From RGB 18-98-98, WEBSAFE 126262
+        'rs-light-teal': '#0E8285', // From RGB 14-130-133, WEBSAFE 0E8285,
+      },
+      fontFamily:{
+    },
   },
+},
   plugins: [],
-}
-
+};
+export default config;
